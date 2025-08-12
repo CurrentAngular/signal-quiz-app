8057d1cf464c1f1a03f7071af48a46efdad5e56a
Valery Semenenko <gearmobile@gmail.com>
2025-08-12 21:36:31 +0300

    add quiz component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Quiz } from './quiz/quiz';
 
 @Component({
   selector: 'aq-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Quiz],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/quiz/quiz.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aq-quiz',
+  imports: [],
+  templateUrl: './quiz.html',
+  styleUrl: './quiz.scss'
+})
+export class Quiz {}
